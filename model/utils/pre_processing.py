19f157e40e25723ff5609a8c852b723841091e73
AxelV369 <axel.vargas92@live.com>
2023-08-11 18:40:40 -0600

    pre_processing.py creation

--- model/utils/pre_processing.py
+++ model/utils/pre_processing.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+import sys
+import os
 import urllib.request
 import json
-import os
 import numpy as np
 import pandas as pd
 import folium
@@ -9,14 +11,12 @@ import matplotlib.colors as mcolors
 import geopandas
 import geopy
 from ortools.linear_solver import pywraplp
-import pandas as pd
-
 from branca.element import Figure
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
-
 import datetime
 from datetime import datetime, timedelta
+
 
 import coloredlogs
 import logging as log
@@ -24,11 +24,41 @@ import webbrowser
 import pycountry
 import math
 
+# # Get the parent directory of the current script (project root)
+project_root = Path(__file__).resolve().parent.parent.parent.parent.parent
+project_root = project_root / "ParcelDeliveryOptimizer"
+# # Add the paths of the relevant directories to sys.path
+sys.path.append(str(project_root))
 
-# C:\Users\z004d5wh\Documents\Master_Thesis\thesis_project\GBS\2_data
-# general_path='C:/Users/z004d5wh/Documents/Master_Thesis/thesis_project/Data'
-# raw_data_path=os.path.join(general_path, 'raw_data.csv')
-# complete_data=data_pre_process(raw_data_path, ';', True)
+
+from project_utils import *
+
+
+# Define the path to the data directory
+data_directory = os.path.join(project_root, "data")
+complete_data=data_pre_process(data_directory, ';', True)
+
+
+# Data Filtering
+drop_dup = False # If true we leave only one address on the period
+
+data_filtered=data_filter(complete_data,'33000','FCA','All', [3], [2021],
+                         ['Siemens Mobility GmbH'], 'Krauss-Maffei-Str.', drop_dup)
+data_filtered.reset_index(drop=True, inplace=True)
 
 
 
+# Time Standarization
+# # Create one date-time column for pick-ups
+node_df['Requested Loading'] = node_df['Requested Loading Date'] + ' ' + node_df['Requested Loading Time']
+node_df['Requested Loading'] = pd.to_datetime(node_df['Requested Loading'])
+
+node_df['Requested Delivery'] = node_df['Requested Delivery Date'] + ' ' + node_df['Requested Delivery Time']
+node_df['Requested Delivery'] = pd.to_datetime(node_df['Requested Delivery'])
+
+
+
+# Storing
+# # Geocoding --------------------------------------------------------------------------------
+df_geocoded_test = geo_coding(node_df, True)
+df_geocoded_test.to_csv('C:/Users/z004d5wh/Documents/Master_Thesis/thesis_project/Data/df_dup_geocoded_202103.csv', index=False)
